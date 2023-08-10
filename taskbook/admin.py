75907c971251b62049485b550e84c2f09872324b
rameshbhatta <rameshjbhatta@gmail.com>
2023-08-10 14:09:45 +0545

    created models

--- taskbook/admin.py
+++ taskbook/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from taskbook.models import *
 
 # Register your models here.
+class TaskAdminView(admin.ModelAdmin):
+    fields=['id','taskname','location','mobile','timedate','details']
+   
+
+class UserAdminView(admin.ModelAdmin):
+    fields=['id','name','username','email','mobile','password']    
+
+admin.site.register(TaskInfo,TaskAdminView)
+admin.site.register(UserInfo,UserAdminView)
--- taskbook/models.py
+++ taskbook/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class UserInfo(models.Model):
+        name=models.CharField(max_length=250,null=False,blank=False)
+        username=models.CharField(max_length=250,unique=True,null=False,blank=False)
+        email=models.EmailField(max_length=250,unique=True,null=False,blank=False)
+        mobile=models.IntegerField(null=True,blank=True)
+        password=models.CharField(max_length=100,null=False,blank=False)
+
+class TaskInfo(models.Model):
+      taskname=models.CharField(max_length=250)  
+      location=models.CharField(max_length=250) 
+      mobile=models.IntegerField()
+      timedate=models.DateTimeField() 
+      details=models.TextField(max_length=400) 
