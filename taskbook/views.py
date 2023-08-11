c08b4907cf97e3f19e3241f17234a6cb4c82f248
Ramesh <rameshjbhatta@gmail.com>
2023-08-11 20:42:38 +0545

    views logics

--- taskbook/views.py
+++ taskbook/views.py
@@ -2,31 +2,25 @@ from django.shortcuts import redirect, render
 from taskbook.models import *
 from django.contrib.auth import authenticate,login,logout
 
-# login authentication
+
+
 def login(request):
     if request.method=='POST':
         #stores the username given in frontend to nusername variable
         nusername=request.POST['username']  
         npassword=request.POST['password']
-        try:
-            #select the user object containing the given username and password
-            user = UserInfo.objects.get(username=nusername , password=npassword) 
-            return render(request,'taskbook/home.html',{'user':user})
-        except UserInfo.DoesNotExist:
-            error_message='Invalid credintial'
-            return render(request,'taskbook/login.html',{'error_message':error_message})
-    return render(request, 'taskbook/login.html')
+        user=authenticate(request,username=nusername,password=npassword)
+        if user is not None:
+            login(request,user)
+            return redirect('homepage')
+    return render('request','taskbook/login.html')
 
-# def alternatelogin(request):
-#      if request.method=='POST':
-#         #stores the username given in frontend to nusername variable
-#         nusername=request.POST['username']  
-#         npassword=request.POST['password']
-#      user=authenticate(request,username=nusername,password=npassword)
-#         if user is not None:
-#             login(request,user)
-#             return redirect('homepage')
 
+# django logout function flushes out the users session and logout the active user
+def logout(request):
+    logout(request)
+    return redirect('homepage')
+    
 
 #create users or handle signup
 def signup(request):
@@ -46,3 +40,24 @@ def create(request):
 
 def home(request):
     return render(request,'taskbook/home.html')
+
+
+
+
+
+
+
+#alternative method to login authentication
+# def login(request):
+#     if request.method=='POST':
+#         #stores the username given in frontend to nusername variable
+#         nusername=request.POST['username']  
+#         npassword=request.POST['password']
+#         try:
+#             #select the user object containing the given username and password
+#             user = UserInfo.objects.get(username=nusername , password=npassword) 
+#             return render(request,'taskbook/home.html',{'user':user})
+#         except UserInfo.DoesNotExist:
+#             error_message='Invalid credintial'
+#             return render(request,'taskbook/login.html',{'error_message':error_message})
+#     return render(request, 'taskbook/login.html')
